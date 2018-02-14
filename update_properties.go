3fe1628b9cc7a8997b65ab5a4f88d42a25f647fb
ehime <dodomeki@gmail.com>
2018-02-13 18:16:39 -0800

    added config iterate

--- update_properties.go
+++ update_properties.go
@@ -2,34 +2,107 @@ package main
 
 import (
   "strings"
-  "log"
+  "bufio"
+  "fmt"
   "flag"
   "os"
+  "io"
+  "bytes"
 )
 
 var (
-  filename string
+  filename  string
 )
 
 func isCommentOrBlank(line string) bool {
   return strings.HasPrefix(line, "#") || "" == strings.TrimSpace(line)
 }
 
-
-func Exists(filename string) bool {
+func fileExists(filename string) bool {
   if _, err := os.Stat(filename); err == nil {
   	return true
   }
   return false
 }
 
+func limitLength(s string, length int) string {
+  if len(s) < length {
+    return s
+  }
+  return s[:length]
+}
+
+
+func ingest(filename string) (err error) {
+  file, err := os.Open(filename)
+  defer file.Close()
+
+  if err != nil {
+    return err
+  }
+
+  reader := bufio.NewReader(file) // Start reading from the file with a reader.
+
+  for {
+    var buffer bytes.Buffer
+    var l []byte
+    var isPrefix bool
+
+    for {
+      l, isPrefix, err = reader.ReadLine()
+      buffer.Write(l)
+
+      if !isPrefix || err != nil { // If we've reached the end of the line or we're at the EOF, break
+        break
+      }
+    }
+
+    if err == io.EOF {
+      break
+    }
+
+    line := buffer.String()
+    fmt.Printf(" > Read %d characters\n", len(line))
+    fmt.Println(" > > " + limitLength(line, 50))      // Process the line here.
+  }
+
+  if err != io.EOF {
+    fmt.Printf(" > Failed!: %v\n", err)
+  }
+  return
+}
 
 func main() {
-  var filename string
+
   flag.StringVar(&filename, "f", "filename", "The file location to be parsed")
   flag.Parse()
 
-  if !Exists(filename) {
-    log.Fatal("File does not exist")
+  if !fileExists(filename) {
+    fmt.Println(" > File does not exist\n")
   }
+  ingest(filename)
+
+  // ## needs to go up
+  //   lineString  := string(bytes)
+  //   pattern     := regexp.MustCompile(`^#([^=]+)=(.+)$`)
+  //
+  //   for _, match := range pattern.FindAllStringSubmatch(lineString, -1) {
+  //     fmt.Printf("key=%s, value=%s\n", match[1], match[2])
+  //   }
+  //
+  //   // for _, element := range RegSplit(lineString, "/^#([^=]+)=(.+)$/") {
+  //   //   name  := element[1]
+  //   //   value := element[2]
+  //   //
+  //   //   fmt.Printf("Name  %v\n", name)
+  //   //   fmt.Printf("Value %v\n", value)
+  //   // }
+  //
+  //   if (isCommentOrBlank(lineString)) {
+  //     file.Seek(int64(-(len(lineString))), 1)
+  //     file.WriteString(line)
+  //   }
+  //
+  //     fmt.Printf(lineString + "\n")
+  // }
 }
