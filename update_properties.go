555eb0936e2d6d7549e1ff7d8d8a72cd30c53bef
ehime <dodomeki@gmail.com>
2018-02-13 20:53:37 -0800

    not writing but otherwise working

--- update_properties.go
+++ update_properties.go
@@ -4,6 +4,7 @@ import (
   "strings"
   "bufio"
   "fmt"
+  "regexp"
   "flag"
   "os"
   "io"
@@ -32,9 +33,17 @@ func limitLength(s string, length int) string {
   return s[:length]
 }
 
+func padWithSpace(str, pad string, length int) string {
+	for {
+		str += pad
+		if len(str) > length {
+			return str[0:length]
+		}
+	}
+}
 
 func ingest(filename string) (err error) {
-  file, err := os.Open(filename)
+  file, err := os.OpenFile(filename, os.O_RDWR, 0666)
   defer file.Close()
 
   if err != nil {
@@ -52,18 +61,52 @@ func ingest(filename string) (err error) {
       l, isPrefix, err = reader.ReadLine()
       buffer.Write(l)
 
-      if !isPrefix || err != nil { // If we've reached the end of the line or we're at the EOF, break
+      if !isPrefix { // If we've reached the end of the line or we're at the EOF, break
         break
       }
     }
 
-    if err == io.EOF {
+    if err == io.EOF || err != nil {
       break
     }
 
-    line := buffer.String()
-    fmt.Printf(" > Read %d characters\n", len(line))
-    fmt.Println(" > > " + limitLength(line, 50))      // Process the line here.
+    line    := buffer.String()
+    pattern := regexp.MustCompile(`^#([^=]+)=(.+)$`)
+    matches := pattern.FindAllStringSubmatch(line, -1) // matches is [][]string
+
+    for _, string := range matches {
+      n := strings.Replace(strings.TrimSpace(string[1]), "#", "", -1)
+      v := strings.TrimSpace(string[2])
+      e := strings.ToLower(strings.Replace(n, ".", "/", -1))
+
+      // newline creation for if block
+      nl := "{{if exists \"/" + e + "\" -}}\n"
+      nl += padWithSpace(n, " ", 80) + " = {{getv \"/" + e + "\" \"" + v + "\"}}\n"
+      nl += "{{end -}}"
+
+      fmt.Println(nl)
+      buffer.WriteString(nl)
+      file.Sync()
+    }
+
+    // dont proccess comments
+    if (isCommentOrBlank(line)) {
+      if (!strings.Contains(line, "=")) {
+        fmt.Println(line)
+        buffer.WriteString(line)
+        file.Sync()
+      }
+    } else {
+      a := strings.Split(line, "=")
+      n := strings.TrimSpace(a[0])
+      v := strings.TrimSpace(a[1])
+      e := strings.ToLower(strings.Replace(n, ".", "/", -1))
+
+      nl := padWithSpace(n, " ", 80) + " = {{getv \"/" + e + "\" \"" + v + "\"}}"
+      fmt.Println(nl)
+      buffer.WriteString(nl)
+      file.Sync()
+    }
   }
 
   if err != io.EOF {
@@ -73,36 +116,12 @@ func ingest(filename string) (err error) {
 }
 
 func main() {
-
   flag.StringVar(&filename, "f", "filename", "The file location to be parsed")
   flag.Parse()
 
   if !fileExists(filename) {
     fmt.Println(" > File does not exist\n")
   }
-  ingest(filename)
 
-  // ## needs to go up
-  //   lineString  := string(bytes)
-  //   pattern     := regexp.MustCompile(`^#([^=]+)=(.+)$`)
-  //
-  //   for _, match := range pattern.FindAllStringSubmatch(lineString, -1) {
-  //     fmt.Printf("key=%s, value=%s\n", match[1], match[2])
-  //   }
-  //
-  //   // for _, element := range RegSplit(lineString, "/^#([^=]+)=(.+)$/") {
-  //   //   name  := element[1]
-  //   //   value := element[2]
-  //   //
-  //   //   fmt.Printf("Name  %v\n", name)
-  //   //   fmt.Printf("Value %v\n", value)
-  //   // }
-  //
-  //   if (isCommentOrBlank(lineString)) {
-  //     file.Seek(int64(-(len(lineString))), 1)
-  //     file.WriteString(line)
-  //   }
-  //
-  //     fmt.Printf(lineString + "\n")
-  // }
+  ingest(filename)
 }
